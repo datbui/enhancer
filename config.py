18c6bd0dce53a7a73ea4404f192f28ab479ad19e
datbui <buidat91@yahoo.com>
2018-02-13 00:26:13 -0600

    Remove unused code

--- config.py
+++ config.py
@@ -14,7 +14,6 @@ flags.DEFINE_string("tfrecord_dir", "tfrecords", "Directory name to store the TF
 flags.DEFINE_integer("batch_size", 10, "The size of batch images [10]")
 flags.DEFINE_integer("image_size", 256, "The size of image to use (will be center cropped) [256]")
 flags.DEFINE_integer("color_channels", 1, "The number of image color channels")
-flags.DEFINE_integer("train_size", np.inf, "The size of train images [np.inf]")
 flags.DEFINE_integer("epoch", 1500, "Epoch to train [1000]")
 flags.DEFINE_float("learning_rate", 1e-4, "The learning rate of gradient descent algorithm [1e-4]")
 flags.DEFINE_string("device", 'CPU', "The device: CPU or GPU [CPU]")
