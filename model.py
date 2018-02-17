f259079434bd5f0532a5d78973ce307ec3a07ffe
datbui <buidat91@yahoo.com>
2018-02-17 00:33:04 -0600

    Implement Dong's FSCNN

--- model.py
+++ model.py
@@ -73,37 +73,55 @@ def model_fn(features, labels, mode, params):
 
 
 def srcnn(lr_images, pkeep_conv=1.0, devices=['/device:CPU:0']):
-    filters_shape = [2, 1, 3, 2, 1]
+    filters_shape = [5, 1, 3, 3, 3, 3, 1, 9]
+    filters = [56, 12, 12, 12, 12, 12, 56]
     channels = 1
     for d in devices:
         with tf.device(d):
             with tf.name_scope('weights'):
-                w1 = tf.Variable(tf.random_normal([filters_shape[0], filters_shape[0], channels, 64], stddev=1e-3), name='cnn_w1')
-                w2 = tf.Variable(tf.random_normal([filters_shape[1], filters_shape[1], 64, 32], stddev=1e-3), name='cnn_w2')
-                w3 = tf.Variable(tf.random_normal([filters_shape[2], filters_shape[2], 32, 16], stddev=1e-3), name='cnn_w3')
-                w4 = tf.Variable(tf.random_normal([filters_shape[3], filters_shape[3], 16, 8], stddev=1e-3), name='cnn_w4')
-                w5 = tf.Variable(tf.random_normal([filters_shape[4], filters_shape[4], 8, channels], stddev=1e-3), name='cnn_w5')
+                w1 = tf.Variable(tf.random_normal([filters_shape[0], filters_shape[0], channels, filters[0]], stddev=1e-3), name='cnn_w1')
+                w2 = tf.Variable(tf.random_normal([filters_shape[1], filters_shape[1], filters[0], filters[1]], stddev=1e-3), name='cnn_w2')
+                w3 = tf.Variable(tf.random_normal([filters_shape[2], filters_shape[2], filters[1], filters[2]], stddev=1e-3), name='cnn_w3')
+                w4 = tf.Variable(tf.random_normal([filters_shape[3], filters_shape[3], filters[2], filters[3]], stddev=1e-3), name='cnn_w4')
+                w5 = tf.Variable(tf.random_normal([filters_shape[4], filters_shape[4], filters[3], filters[4]], stddev=1e-3), name='cnn_w5')
+                w6 = tf.Variable(tf.random_normal([filters_shape[5], filters_shape[5], filters[4], filters[5]], stddev=1e-3), name='cnn_w6')
+                w7 = tf.Variable(tf.random_normal([filters_shape[6], filters_shape[6], filters[5], filters[6]], stddev=1e-3), name='cnn_w7')
+                w8 = tf.Variable(tf.random_normal([filters_shape[7], filters_shape[7], filters[6], channels], stddev=1e-3), name='cnn_w8')
             with tf.name_scope('biases'):
-                b1 = tf.Variable(tf.zeros([64]), name='cnn_b1')
-                b2 = tf.Variable(tf.zeros([32]), name='cnn_b2')
-                b3 = tf.Variable(tf.zeros([16]), name='cnn_b3')
-                b4 = tf.Variable(tf.zeros([8]), name='cnn_b4')
-                b5 = tf.Variable(tf.zeros([channels]), name='cnn_b5')
+                b1 = tf.Variable(tf.zeros(filters[0]), name='cnn_b1')
+                b2 = tf.Variable(tf.zeros(filters[1]), name='cnn_b2')
+                b3 = tf.Variable(tf.zeros(filters[2]), name='cnn_b3')
+                b4 = tf.Variable(tf.zeros(filters[3]), name='cnn_b4')
+                b5 = tf.Variable(tf.zeros(filters[4]), name='cnn_b5')
+                b6 = tf.Variable(tf.zeros(filters[5]), name='cnn_b6')
+                b7 = tf.Variable(tf.zeros(filters[6]), name='cnn_b7')
+                b8 = tf.Variable(tf.zeros([channels]), name='cnn_b8')
             with tf.name_scope('predictions'):
                 conv1 = tf.nn.bias_add(tf.nn.conv2d(lr_images, w1, strides=[1, 1, 1, 1], padding='SAME'), b1, name='conv_1')
-                conv1r = tf.nn.relu(conv1, name='relu_1')
-                conv1r = tf.nn.dropout(conv1r, pkeep_conv)
+                conv1r = _parametric_relu(conv1, name='relu_1')
                 conv2 = tf.nn.bias_add(tf.nn.conv2d(conv1r, w2, strides=[1, 1, 1, 1], padding='SAME'), b2, name='conv_2')
-                conv2r = tf.nn.relu(conv2, name='relu_2')
-                conv2r = tf.nn.dropout(conv2r, pkeep_conv)
+                conv2r = _parametric_relu(conv2, name='relu_2')
                 conv3 = tf.nn.bias_add(tf.nn.conv2d(conv2r, w3, strides=[1, 1, 1, 1], padding='SAME'), b3, name='conv_3')
-                conv3r = tf.nn.relu(conv3, name='relu_3')
-                conv3r = tf.nn.dropout(conv3r, pkeep_conv)
+                conv3r = _parametric_relu(conv3, name='relu_3')
                 conv4 = tf.nn.bias_add(tf.nn.conv2d(conv3r, w4, strides=[1, 1, 1, 1], padding='SAME'), b4, name='conv_4')
-                conv4r = tf.nn.relu(conv4, name='relu_4')
+                conv4r =_parametric_relu(conv4, name='relu_4')
                 conv5 = tf.nn.bias_add(tf.nn.conv2d(conv4r, w5, strides=[1, 1, 1, 1], padding='SAME'), b5, name='conv_5')
-                predictions = conv5
+                conv5r = _parametric_relu(conv5, name='relu_5')
+                conv6 = tf.nn.bias_add(tf.nn.conv2d(conv5r, w6, strides=[1, 1, 1, 1], padding='SAME'), b6, name='conv_6')
+                conv6r = _parametric_relu(conv6, name='relu_6')
+                conv7 = tf.nn.bias_add(tf.nn.conv2d(conv6r, w7, strides=[1, 1, 1, 1], padding='SAME'), b7, name='conv_7')
+                conv7r = _parametric_relu(conv7, name='relu_7')
+                conv8 = tf.nn.bias_add(tf.nn.conv2d(conv7r, w8, strides=[1, 1, 1, 1], padding='SAME'), b8, name='conv_8')
+                predictions = conv8
     return predictions
+
+
+def _parametric_relu(_x, name):
+    alphas = tf.Variable(initial_value=0.1)
+    pos = tf.nn.relu(_x)
+    neg = alphas * (_x - abs(_x)) * 0.5
+    prelu = tf.Variable((pos + neg), name=name)
+    return prelu
 
 
 def _tf_fspecial_gauss(size, sigma):
