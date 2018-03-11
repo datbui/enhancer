f4dfd971120b3574a632f894ea1656136a3d43bf
datbui <buidat91@yahoo.com>
2018-03-11 15:37:03 -0500

    Change configuration

--- scripts/start-testing-local.sh
+++ scripts/start-testing-local.sh
@@ -3,6 +3,6 @@
 echo 'Run testing....'
 pwd
 source ~/tensorflow/bin/activate
-python3 main.py --is_train=false --dataset=test
+python3 main.py --is_train=false --dataset=images_cleaned
 deactivate
 echo 'Testing has been completed'
